4ffbe1791bf2fdc7c55c602395585184d952356f
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-27 08:48:57 -0500

    first commit

--- ZaHunter/MPRViewController.m
+++ ZaHunter/MPRViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "MPRViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
-@interface MPRViewController ()
+@interface MPRViewController () <UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate>
+{
+    NSArray     *pizzaJoints;
 
+}
+@property (strong, nonatomic) IBOutlet UITableView *myTableView;
+@property CLLocationManager *locationManager;
 @end
 
 @implementation MPRViewController
@@ -17,13 +24,99 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.locationManager = [CLLocationManager new];
+    self.locationManager.delegate = self;
+    
+    //[self.myTableView reloadData];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)locationManager: (CLLocationManager *)manager didFailWithError:(NSError *)error
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSLog(@"%@",error);
 }
+
+- (void)locationManager: (CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    for (CLLocation *location in locations)
+    {
+        if (location.verticalAccuracy < 1000 && location.horizontalAccuracy < 1000)
+        {
+            //self.myLabel.text = @"Location found, Reverse geocoding...";
+            [self startReverseGeoCode:location];
+            [self.locationManager stopUpdatingLocation];
+            break;
+        }
+    }
+}
+
+- (void)startReverseGeoCode: (CLLocation*) location
+{
+    CLGeocoder *geocoder = [CLGeocoder new];
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        //self.myLabel.text = [NSString stringWithFormat:@"%@", placemarks.firstObject];
+        [self findPizzaJoints:placemarks.firstObject];
+    }];
+}
+
+- (void)findPizzaJoints: (CLPlacemark *)placemark
+{
+    self.title = @"Pizza Joints";
+    MKLocalSearchRequest* request = [MKLocalSearchRequest new];
+    request.naturalLanguageQuery = @"Pizza";
+    request.region = MKCoordinateRegionMake(placemark.location.coordinate, MKCoordinateSpanMake(1, 1));
+    
+    MKLocalSearch* search = [[MKLocalSearch alloc] initWithRequest:request];
+    [search startWithCompletionHandler:^(MKLocalSearchResponse *response, NSError *error)
+     {
+         NSArray *mapItems = response.mapItems;
+         pizzaJoints = mapItems;
+         [self.myTableView reloadData];
+     }];
+}
+
+- (void)showDirections: (MKMapItem*) destinationMapItem
+{
+    MKDirectionsRequest *request = [MKDirectionsRequest new];
+    request.source = [MKMapItem mapItemForCurrentLocation];
+    request.destination = destinationMapItem;
+    
+    MKDirections *directions = [[MKDirections alloc] initWithRequest:request];
+    [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+        MKRoute *route = response.routes.firstObject;
+        //self.myLabel.text = @"";
+        
+//        for (MKRouteStep *step in route.steps)
+//        {
+//            self.myLabel.text = [NSString stringWithFormat:@"%@\n%@", self.myLabel.text, step.instructions];
+//        }
+    }];
+}
+
+-(NSInteger)tableView: (UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [pizzaJoints count];
+}
+
+-(UITableViewCell*)tableView: (UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Returns table cell containing information from the MeetUpEvent
+    
+    UITableViewCell *cell            = [tableView dequeueReusableCellWithIdentifier:@"PizzaCellID"];
+    MKMapItem *pizzaPlaces           = pizzaJoints[indexPath.row];
+    
+    int distance = roundf([pizzaPlaces.placemark.location distanceFromLocation:self.locationManager.location]);
+    NSString *dist= [NSString stringWithFormat:@"Crow's Distance:  %i meters", distance];
+    
+    cell.textLabel.text = pizzaPlaces.name;
+    cell.detailTextLabel.text = dist;
+    
+    return cell;
+}
+
+- (IBAction)pizzaSearch:(id)sender
+{
+    [self.locationManager startUpdatingLocation];
+}
+
 
 @end
